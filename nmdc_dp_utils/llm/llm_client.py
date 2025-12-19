95089c4a459b799f8ab5aa894d2a27c34cce3668
olivia.hess <olivia.hess@pnnl.gov>
2025-12-19 13:17:16 -0500

    update to use agent sdk

--- nmdc_dp_utils/llm/llm_client.py
+++ nmdc_dp_utils/llm/llm_client.py
@@ -3,10 +3,14 @@ This file is for LLM components and related functionalities.
 """
 
 # OpenAI imports
-from openai import OpenAI
+from agents import Agent, Runner, set_tracing_disabled, OpenAIResponsesModel
+from agents.mcp import MCPServerStdio, MCPServerStdioParams
+from openai import AsyncOpenAI
+
 
 # Standard library imports
 import os
+
 
 class LLMClient():
     """
@@ -17,11 +21,44 @@ class LLMClient():
     """
     def __init__(self):
         API_KEY = os.getenv("AI_INCUBATOR_API_KEY")
-        self.model = "claude-3-7-sonnet-20250219-v1-project"
+        self.model_name = "gemini-2.5-flash-project"
         self.base_url = "https://ai-incubator-api.pnnl.gov"
-        client = OpenAI(base_url=self.base_url, api_key=API_KEY)
+        client = AsyncOpenAI(base_url=self.base_url, api_key=API_KEY)
         self.client = client
+        self.model_object = OpenAIResponsesModel(model=self.model_name, openai_client=self.client)
+        self.mcp_servers=[os.path.join(os.path.dirname(__file__), "llm_protocol_context/schema_server.py")]
 
+    async def get_response(self, messages: list):
+        """
+        Get a response from the LLM client.
+
+        Parameters
+        ----------
+            messages (list): A list of messages to send to the model.
+        Returns
+        -------
+            The model's response.
+        """
+        # params to run the mcp server
+        params = MCPServerStdioParams(command="python", args=self.mcp_servers)
+        # tracing is not supported in our AI Incubator instance. Must be disabled.
+        set_tracing_disabled(disabled=True)
+
+        async with MCPServerStdio(
+            params=params,
+            client_session_timeout_seconds=60
+        ) as mcp_server_instance:
+            # use the runner to run the agent with our mcp server and custom model client
+            result = await Runner.run(
+                Agent(
+                    name="Assistant",
+                    instructions="You are a helpful assistant",
+                    mcp_servers=[mcp_server_instance],
+                    model=self.model_object
+                ),
+                input=messages
+            )
+            return result.final_output
 
 
 
