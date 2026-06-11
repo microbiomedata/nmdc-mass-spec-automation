d03b8019ab3ce87a2e98aa400ab20c69aca647eb
bmeluch <beata.meluch@pnnl.gov>
2026-06-10 17:56:46 -0700

    fix client package name

--- nmdc_dp_utils/workflow_manager_mixins.py
+++ nmdc_dp_utils/workflow_manager_mixins.py
@@ -2417,8 +2417,8 @@ class NMDCWorkflowBiosampleManager:
             The CSV file is saved as 'biosample_attributes.csv' in the study's metadata directory.
             This method is automatically skipped if biosample_attributes_fetched trigger is set.
         """
-        from nmdc_api_utilities.biosample_search import BiosampleSearch
-        from nmdc_api_utilities.study_search import StudySearch
+        from nmdc-client.biosample_search import BiosampleSearch
+        from nmdc-client.study_search import StudySearch
 
         if study_ids is None:
             study_ids = self.config["study"]["id"]
